25cff5dfae3563f9638718adc430383df2799ad8
shi.fan <shifan3rd@gmail.com>
2026-01-13 12:00:18 +0000

    fgg

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM cuda12.9-cp311
+
+RUN apt update && apt install -y git libnuma1 libnuma-dev
+RUN git clone https://github.com/shifan3/SpecForge.git -b support-qwen3-vl
+WORKDIR /SpecForge
+RUN python3.11 -m pip install setuptools -U
+RUN pip install -e .
--- examples/run_image2html_eagle3_offline.sh
+++ examples/run_image2html_eagle3_offline.sh
@@ -10,7 +10,7 @@ BUILD_DATASET_NUM_PROC=${BUILD_DATASET_NUM_PROC:-64}
 
 # Dataset paths
 PROJECT_NAME=image2html
-TARGET_MODEL_PATH=/mnt/data5/school_tiku/models/image2html
+TARGET_MODEL_PATH=/mnt/data5/school_tiku/models/image2html_large_3/QUANT/AWQ/W4A16
 RAW_DATASET=/mnt/data5/datasets/真题卷image2html/dataset.json
 CONVERTED_DATASET=/mnt/data5/datasets/真题卷image2html/dataset_draft_specforge.jsonl
 CACHE_DIR=cache/$PROJECT_NAME
@@ -20,10 +20,10 @@ CONFIG_FILE=$ROOT_DIR/configs/qwen3-vl-32b-eagle3.json
 # Convert dataset to SpecForge format (skip if already exists)
 if [ ! -f "$CONVERTED_DATASET" ]; then
     echo "Converting dataset to SpecForge format..."
-    python3.11 $ROOT_DIR/scripts/convert_vlm_dataset.py \
+    python3.10 $ROOT_DIR/scripts/convert_vlm_dataset.py \
         --input "$RAW_DATASET" \
         --output "$CONVERTED_DATASET" \
-        --max-entries 100
+        --max-entries 10000
 
     if [ $? -ne 0 ]; then
         echo "Error: Dataset conversion failed!"
@@ -34,31 +34,29 @@ else
 fi
 
 # Step 1: Generate hidden states (skip if already exists)
-#if [ ! -d "$HIDDEN_STATES_PATH" ] || [ -z "$(ls -A $HIDDEN_STATES_PATH 2>/dev/null)" ]; then
+if [ ! -d "$HIDDEN_STATES_PATH" ] || [ -z "$(ls -A $HIDDEN_STATES_PATH 2>/dev/null)" ]; then
     echo "Generating hidden states..."
     TOKENIZERS_PARALLELISM=false torchrun \
         --standalone \
         --nproc_per_node $NUM_GPUS \
-        $ROOT_DIR/scripts/prepare_hidden_states.py \
+        $ROOT_DIR/scripts/prepare_hidden_states_pytorch.py \
         --target-model-path $TARGET_MODEL_PATH \
         --data-path "$CONVERTED_DATASET" \
         --output-path "$HIDDEN_STATES_PATH" \
         --chat-template qwen3-vl \
         --max-length 8192 \
-        --tp-size $TP_SIZE \
         --batch-size 1 \
         --is-vlm \
         --enable-aux-hidden-states \
-        --sglang-mem-fraction-static 0.85
+        #--sglang-mem-fraction-static 0.85
 
     if [ $? -ne 0 ]; then
         echo "Error: Hidden states generation failed!"
         exit 1
     fi
-#else
-    #echo "Using existing hidden states: $HIDDEN_STATES_PATH"
-#fi
-exit 0
+else
+    echo "Using existing hidden states: $HIDDEN_STATES_PATH"
+fi
 OUTPUT_DIR=$ROOT_DIR/outputs/$PROJECT_NAME-eagle3-offline
 rm -rf $OUTPUT_DIR
 # Step 2: Train eagle3 offline
